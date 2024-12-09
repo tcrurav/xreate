94c696add0074dee1a703e92dc3a95a37805dd8b
tcrurav <tiburcio.cruz@gmail.com>
2024-12-09 03:06:06 +0000

    feat aliens and change room by balls

--- frontend/Assets/Scripts/Models/User.cs
+++ frontend/Assets/Scripts/Models/User.cs
@@ -5,7 +5,10 @@ using System.Collections.Generic;
 public class User
 {
     public int id;
-    public string name;
     public string username;
     public string password;
+    public string nickname;
+    public string code;
+    public string role;
+    public string nationality;
 }
