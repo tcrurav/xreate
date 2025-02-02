61c64a7df7499e7809af36417c0356b60be1e957
tcrurav <tiburcio.cruz@gmail.com>
2025-02-02 12:48:05 +0000

    fix leisure module with idafe's package

--- frontend/Assets/XREATE/SharedAssets/Scripts/Navigation/RoomChangeController.cs
+++ frontend/Assets/XREATE/SharedAssets/Scripts/Navigation/RoomChangeController.cs
@@ -8,22 +8,13 @@ public class RoomChangeController : MonoBehaviour
     private readonly Vector3 OffsetCorridorToRoomModuleB = new(87f, 6.75f, 19f);
     private readonly Vector3 OffsetRoomModuleB = new(87f, 6.75f, 28f);
     private readonly Vector3 OffsetCorridorToLeisureModule = new(99f, 6.75f, 11.5f);
-    private readonly Vector3 OffsetLeisureModule = new(105f, 6.5f, 21.5f);
+    private readonly Vector3 OffsetLeisureModule = new(104f, 6.5f, 16f);
     private readonly Vector3 OffsetBuildingA = new(0, 0, -12);
-
-    //private readonly Vector3 OffsetRoomModuleA = new(-7.5f, 6.5f, -20f);
-    //private readonly Vector3 OffsetCorridorToRoomModuleA = new(-7.5f, 6.75f, -13f);
-    //private readonly Vector3 OffsetCorridorToRoomModuleB = new(-7.5f, 6.75f, 21f);
-    //private readonly Vector3 OffsetRoomModuleB = new(-7.5f, 6.5f, 32f);
-    //private readonly Vector3 OffsetCorridorToLeisureModule = new(4f, 6.75f, 16f);
-    //private readonly Vector3 OffsetLeisureModule = new(9.65f, 6.5f, 19.5f);
-    //private readonly Vector3 OffsetBuildingA = new(-95.15f, 0f, 4.61f);
 
     public void ChangeToRoomModuleA()
     {
         //MainNetworkManager.HideAllStudentsOfOtherTeams();
         //MainNetworkManager.ChangeSceneTo(MainManager.GetUser().id, "RoomModuleAScene");
-        //ChangePosition(OffsetRoomModuleA);
         //ChangePlayerPosition(MainManager.GetUser().id, OffsetBuildingA);
         MainNavigationManager.ChangePosition(OffsetRoomModuleA);
     }
@@ -32,7 +23,6 @@ public class RoomChangeController : MonoBehaviour
     {
         //MainNetworkManager.HideAllStudentsOfOtherTeams();
         //MainNetworkManager.ChangeSceneTo(MainManager.GetUser().id, "TunnelConnectorCScene");
-        //ChangePosition(OffsetCorridorToRoomModuleA);
         //ChangePlayerPosition(MainManager.GetUser().id, OffsetCorridorToRoomModuleA);
         MainNavigationManager.ChangePosition(OffsetCorridorToRoomModuleA);
     }
@@ -41,7 +31,6 @@ public class RoomChangeController : MonoBehaviour
     {
         //MainNetworkManager.HideAllStudentsOfOtherTeams();
         //MainNetworkManager.ChangeSceneTo(MainManager.GetUser().id, "TunnelConnectorDScene");
-        //ChangePosition(OffsetCorridorToRoomModuleB);
         //ChangePlayerPosition(MainManager.GetUser().id, OffsetCorridorToRoomModuleB);
         MainNavigationManager.ChangePosition(OffsetCorridorToRoomModuleB);
     }
@@ -50,7 +39,6 @@ public class RoomChangeController : MonoBehaviour
     {
         //MainNetworkManager.HideAllStudentsOfOtherTeams();
         //MainNetworkManager.ChangeSceneTo(MainManager.GetUser().id, "RoomModuleBScene");
-        //ChangePosition(OffsetRoomModuleB);
         //ChangePlayerPosition(MainManager.GetUser().id, OffsetRoomModuleB);
         //ChangePlayerPosition(MainManager.GetUser().id, new Vector3(100, 100, 100));
         MainNavigationManager.ChangePosition(OffsetRoomModuleB);
@@ -60,7 +48,6 @@ public class RoomChangeController : MonoBehaviour
     {
         //MainNetworkManager.HideAllStudentsOfOtherTeams();
         //MainNetworkManager.ChangeSceneTo(MainManager.GetUser().id, "TunnelConnectorFScene");
-        //ChangePosition(OffsetCorridorToLeisureModule);
         //ChangePlayerPosition(MainManager.GetUser().id, OffsetCorridorToLeisureModule);
         MainNavigationManager.ChangePosition(OffsetCorridorToLeisureModule);
     }
@@ -69,7 +56,6 @@ public class RoomChangeController : MonoBehaviour
     {
         //MainNetworkManager.HideAllStudentsOfOtherTeams();
         //MainNetworkManager.ChangeSceneTo(MainManager.GetUser().id, "LeisureModuleScene");
-        //ChangePosition(OffsetLeisureModule);
         //ChangePlayerPosition(MainManager.GetUser().id, OffsetLeisureModule);
         MainNavigationManager.ChangePosition(OffsetLeisureModule);
     }
@@ -78,42 +64,8 @@ public class RoomChangeController : MonoBehaviour
     {
         //MainNetworkManager.ShowAllStudents();
         //MainNetworkManager.ChangeSceneTo(MainManager.GetUser().id, "MainScene");
-        //ChangePosition(OffsetBuildingA);
         //ChangePlayerPosition(MainManager.GetUser().id, OffsetBuildingA);
         MainNavigationManager.ChangePosition(OffsetBuildingA);
-    }
-
-    private void ChangePosition(Vector3 target)
-    {
-        GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
-        Vector3 offset = camera.transform.position - target;
-
-        GameObject XreatePortMain = GameObject.FindGameObjectWithTag("XreatePortMain");
-        XreatePortMain.transform.position = offset;
-
-        GameObject XreatePortRoomModuleB = GameObject.FindGameObjectWithTag("XreatePortRoomModuleB");
-        XreatePortRoomModuleB.transform.position = offset;
-
-        GameObject XreatePortRoomModuleA = GameObject.FindGameObjectWithTag("XreatePortRoomModuleA");
-        XreatePortRoomModuleA.transform.position = offset;
-
-        GameObject XreatePortLeisureModule = GameObject.FindGameObjectWithTag("XreatePortLeisureModule");
-        XreatePortLeisureModule.transform.position = offset;
-
-        GameObject XreatePortTunnelConnectorC = GameObject.FindGameObjectWithTag("XreatePortTunnelConnectorC");
-        XreatePortTunnelConnectorC.transform.position = offset;
-
-        GameObject XreatePortTunnelConnectorF = GameObject.FindGameObjectWithTag("XreatePortTunnelConnectorF");
-        XreatePortTunnelConnectorF.transform.position = offset;
-
-        GameObject XreatePortTunnelConnectorD = GameObject.FindGameObjectWithTag("XreatePortTunnelConnectorD");
-        XreatePortTunnelConnectorD.transform.position = offset;
-
-        GameObject UI = GameObject.FindGameObjectWithTag("UI");
-        UI.transform.position = camera.transform.position + new Vector3(.5f, -0.5f, .5f);
-
-        GameObject ConnectedUsersSpatialPanelUI = GameObject.FindGameObjectWithTag("ConnectedUsersSpatialPanelUI");
-        ConnectedUsersSpatialPanelUI.transform.position = camera.transform.position + new Vector3(-.5f, -0.5f, .5f);
     }
 
     private void ChangePlayerPosition(int playerId, Vector3 newPosition)
